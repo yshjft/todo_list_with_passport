68dd3514a43a75ca1e40acb544007096363fd352
hong yuseok <jericoh@naver.com>
2020-12-29 01:23:13 +0900

    view 구성 중

--- route/page.js
+++ route/page.js
@@ -1,10 +1,19 @@
 const express = require('express')
 
 const router =  express.Router()
+const TITLE = 'todo-list'
 
 router.get('/', (req, res, next)=>{
     res.render('index', {
-        title: 'todo-list'
+        title: TITLE,
+        user: {id: 123}
+    })
+})
+
+router.get('/join', (req, res, next)=>{
+    res.render('join', {
+        title: TITLE,
+        user: null,
     })
 })
 
